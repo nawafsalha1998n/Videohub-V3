36a64445f64e645ed3d3f55205b9defbc48d1d7e
nawafsalha1998n <pemoral.official@gmail.com>
2025-09-25 16:49:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  i18n: {
+    locales: ['en', 'ar', 'fa', 'es', 'fr', 'de'],
+    defaultLocale: 'en',
+    localeDetection: true
+  }
 };
 
 module.exports = nextConfig;
